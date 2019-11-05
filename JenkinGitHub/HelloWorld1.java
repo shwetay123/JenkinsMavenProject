92c60910792252d701459d8536cadb4f342f7dc2
shwetay123 <41732550+shwetay123@users.noreply.github.com>
2019-11-05 02:11:14 -0800

    Add files via upload

---
+++ JenkinGitHub/HelloWorld1.java
@@ -0,0 +1,10 @@
+public class HelloWorld1 {
+    public static void main(String args[]){
+        System.out.println("I'm dancing......................");
+	System.out.println("I'm dancing,.........................");
+System.out.println("I'm dancing........................");
+System.out.println("I'm dancing............");
+System.out.println("I'm dancing.........");	
+System.out.println("I'm dancing........");
+    }
+}
